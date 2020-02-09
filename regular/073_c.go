b1d87789994078209872e0f3b4346e3966a196cb
y-zumi <b1501498@planet.kanazawa-it.ac.jp>
2020-02-09 13:49:14 +0900

    add todo comment

--- regular/073_c.go
+++ regular/073_c.go
@@ -6,6 +6,7 @@ func main() {
 	var N, T int
 	fmt.Scan(&N, &T)
 
+	// TODO: bufferで入出力すれば1000msぐらい早くなる
 	ts := make([]int, N)
 	for i := range ts {
 		fmt.Scan(&ts[i])
@@ -23,27 +24,3 @@ func main() {
 	}
 	fmt.Println(ans + T)
 }
-
-// func f(t1, t2, T int) int {
-// 	// diff := abs(t1, t2)
-// 	diff := t2 - t1
-// 	if diff >= T {
-// 		return T
-// 	}
-// 	return diff
-// }
-
-// func abs(x, y int) int {
-// 	a := x - y
-// 	if a < 0 {
-// 		return a * -1
-// 	}
-// 	return a
-// }
-
-// t1 - t2 >= T
-// 通常加算
-// t1 - t2 < T
-// t1に関してdiffだけを追加する
-// 重複部分を取り除く
-// t1=3, t2=5, diff=2, T=10, R=12
