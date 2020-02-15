de5c72eaa72619fa928575e3a7f916b9bfe8344b
y-zumi <b1501498@planet.kanazawa-it.ac.jp>
2020-02-15 21:03:27 +0900

    wa abc 152 c

--- beginner/152_c.go
+++ beginner/152_c.go
@@ -11,31 +11,20 @@ func main() {
 	}
 
 	var ans int
-	var low bool
 	for i := len(p) - 1; i >= 0; i-- {
-		low = true
-		for j := i; j >= 0; j-- {
-			if p[i] > p[j] {
-				low = false
-				break
-			}
-		}
-		if low == true {
+		if min(p[:i+1]) == p[i] {
 			ans++
 		}
 	}
 	fmt.Println(ans)
+}
 
-	// for j := 0; j < len(p); j++ {
-	// 	for i := j; i < len(p); i++ {
-	// 		if p[i] > p[j] {
-	// 			// copy(p[i:], p[i+1:])
-	// 			// p[len(p)-1] = 0
-	// 			// p = p[:len(p)-1]
-	// 			p = append(p[:i], p[i+1:]...)
-	// 			i--
-	// 		}
-	// 	}
-	// }
-	// fmt.Println(len(p))
+func min(array []int) int {
+	var m int
+	for i, v := range array {
+		if i == 0 || v < m {
+			m = v
+		}
+	}
+	return m
 }
