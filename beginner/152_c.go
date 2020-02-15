4bbdaf7b6d039cae4adcb548957068767045e238
y-zumi <b1501498@planet.kanazawa-it.ac.jp>
2020-02-15 21:07:46 +0900

    ac abc 152 c

--- beginner/152_c.go
+++ beginner/152_c.go
@@ -10,21 +10,14 @@ func main() {
 		fmt.Scan(&p[i])
 	}
 
-	var ans int
-	for i := len(p) - 1; i >= 0; i-- {
-		if min(p[:i+1]) == p[i] {
+	var m, ans int
+	for i, v := range p {
+		if i == 0 || v < m {
+			m = v
+		}
+		if p[i] == m {
 			ans++
 		}
 	}
 	fmt.Println(ans)
-}
-
-func min(array []int) int {
-	var m int
-	for i, v := range array {
-		if i == 0 || v < m {
-			m = v
-		}
-	}
-	return m
 }
