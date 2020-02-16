690b5f3927cfa0698d5d812daeff0fa91373c507
y-zumi <b1501498@planet.kanazawa-it.ac.jp>
2020-02-16 15:34:01 +0900

    refactor abc154d

--- beginner/154_d.go
+++ beginner/154_d.go
@@ -13,55 +13,32 @@ func main() {
 	idx := make([]float64, 0, k)
 	maxIdx := make([]float64, k)
 	var max, sum float64
-	var head int
 	for i := 0; i < n; i++ {
 		sum += p[i]
 		idx = append(idx, p[i])
-		if i >= k-1 {
-			// fmt.Println(sum, idx)
+		if len(idx) > k {
+			sum -= idx[0]
+			idx = idx[1:]
+		}
+		if len(idx) == k {
 			if max < sum {
 				max = sum
 				copy(maxIdx, idx)
 			}
-			sum -= p[head]
-			idx = idx[1:]
-			head++
 		}
 	}
-
-	// fmt.Println(max, maxIdx)
-
-	// indexes := make([]float64, k)
-	// max := 0
-	// maxIndexes := make([]float64, k)
-	// for i := 0; i <= n-k; i++ {
-	// 	var sum int
-	// 	for j := 0; j < k; j++ {
-	// 		sum += p[i+j]
-	// 		indexes[j] = float64(p[i+j])
-	// 	}
-	// 	// fmt.Println(max, sum, indexes)
-	// 	if max < sum {
-	// 		max = sum
-	// 		copy(maxIndexes, indexes)
-	// 	}
-	// }
-
-	// // fmt.Println(max, maxIndexes)
 
 	sum = 0.0
 	for _, v := range maxIdx {
 		sum += expectation(v)
 	}
 	fmt.Printf("%.12f\n", sum)
-
 }
 
 func expectation(p float64) float64 {
-	ratio := 1.0 / float64(p)
 	var sum float64
 	for i := 1.0; i <= p; i++ {
-		sum += i * ratio
+		sum += i
 	}
-	return sum
+	return sum / p
 }
