2317c5dfe06faa4716ac0d5e0108328d20924e97
y-zumi <b1501498@planet.kanazawa-it.ac.jp>
2020-06-11 01:04:15 +0900

    refactor(beginner): refactor beginner 079 C

--- beginner/079/079_c_2.go
+++ beginner/079/079_c_2.go
@@ -13,25 +13,31 @@ func main() {
 		v[i] = int(r - '0')
 	}
 
-	n := 3
-	for bit := 0; bit < (1 << uint(n)); bit++ {
-		ans := v[0]
-		ansStr := fmt.Sprint(v[0])
+	n := len(input) - 1
+	op := make([]string, n)
+	var ans int
+	wantAns := 7
 
+	for bit := 0; bit < (1 << uint(n)); bit++ {
+		ans = v[0]
 		for i := 0; i < n; i++ {
 			if bit&(1<<uint(i)) == (1 << uint(i)) {
 				ans += v[i+1]
-				ansStr = fmt.Sprintf("%s+%d", ansStr, v[i+1])
+				op[i] = "+"
 			} else {
 				ans -= v[i+1]
-				ansStr = fmt.Sprintf("%s-%d", ansStr, v[i+1])
+				op[i] = "-"
 			}
 		}
-
-		if ans == 7 {
-			ansStr = fmt.Sprintf("%s=%d", ansStr, ans)
-			fmt.Println(ansStr)
-			return
+		if ans == wantAns {
+			break
 		}
 	}
+
+	var ansFormula string
+	for i := range op {
+		ansFormula = fmt.Sprintf("%s%d%s", ansFormula, v[i], op[i])
+	}
+	ansFormula = fmt.Sprintf("%s%d=%d", ansFormula, v[len(v)-1], wantAns)
+	fmt.Println(ansFormula)
 }
